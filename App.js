3c433f7ebad5f1fd33e6153bb2a893d80ee99944
Guillermo Jimenez <guillermojimenez@GuillermosMBP2.fios-router.home>
2022-07-25 00:45:32 -0400

    layouts

--- App.js
+++ App.js
@@ -1,28 +1,22 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button } from 'react-native';
+import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <View><Text>Another text</Text></View>
-      <Text style={styles.dummyText}>Hello world!</Text>
-      <Button title='press me'></Button>
-      <StatusBar style="auto" />
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder='Your course goal!'/>
+        <Button title='Add Goal'/>
+      </View>
+      <View>
+        <Text>List of goals...</Text>
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  dummyText: {
-    margin: 10,
-    padding: 10,
-    borderWidth: 2,
-    borderColor: 'blue',
+  appContainer: {
+    padding: 50
   }
 });
