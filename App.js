95ab73862211ba3902035ec9cb39994f1dd14959
Guillermo Jimenez <guillermojimenez@GuillermosMBP2.fios-router.home>
2022-07-25 21:40:32 -0400

    flatlist

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import {
   Button,
   TextInput,
   ScrollView,
+  FlatList,
   ß,
 } from "react-native";
 import { useState } from "react";
@@ -20,7 +21,7 @@ export default function App() {
   function addGoalHandler() {
     setCourseGoals((currentCourseGoals) => [
       ...currentCourseGoals,
-      enteredGoalText,
+      {text: enteredGoalText, id:Math.random().toString()},
     ]);
   }
 
@@ -35,13 +36,20 @@ export default function App() {
         <Button title="Add Goal" onPress={addGoalHandler} />
       </View>
       <View style={styles.goalsContainer}>
-        <ScrollView alwaysBounceVertical = {false}>
-          {courseGoals.map((goal) => (
-            <View style={styles.goalItem} key={goal}>
-              <Text style={styles.goalText}>{goal}</Text>
-            </View>
-          ))}
-        </ScrollView>
+        <FlatList
+          data={courseGoals}
+          alwaysBounceVertical={false}
+          renderItem={(itemData) => {
+            return (
+              <View style={styles.goalItem}>
+                <Text style={styles.goalText}>{itemData.item.text}</Text>
+              </View>
+            )
+          }}
+          keyExtractor={(item, index)=> {
+            return item.id
+          }}
+        />
       </View>
     </View>
   );
