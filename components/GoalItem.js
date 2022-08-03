feddff9e4e38704416b016e2e65f27113f6f7c6c
Guillermo Jimenez <guillermojimenez@Guillermos-MacBook-Pro-2.local>
2022-08-03 18:29:01 -0400

    press style feedback

--- components/GoalItem.js
+++ components/GoalItem.js
@@ -2,11 +2,11 @@ import { StyleSheet, View, Text, Pressable } from "react-native";
 
 function GoalItem(props) {
   return (
-    <Pressable onPress={props.onDeleteItem.bind(this, props.id)}>
-      <View style={styles.goalItem}>
+    <View style={styles.goalItem}>
+      <Pressable onPress={props.onDeleteItem.bind(this, props.id)} style={({pressed})=>pressed&&styles.pressedItem}>
         <Text style={styles.goalText}>{props.text}</Text>
-      </View>
-    </Pressable>
+      </Pressable>
+    </View>
   );
 }
 
@@ -17,9 +17,13 @@ const styles = StyleSheet.create({
     margin: 8,
     borderRadius: 6,
     backgroundColor: "#5e0acc",
-    padding: 8,
+    
+  },
+  pressedItem:{
+    opacity: 0.5,
   },
   goalText: {
     color: "white",
+    padding: 8,
   },
 });
